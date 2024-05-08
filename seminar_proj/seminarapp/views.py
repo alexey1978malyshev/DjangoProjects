6b8636f12351e4ebce544ffccaa5f1f1c64b0ca1
A9219 <alexey1978malyshev@gmail.com>
2024-05-08 12:57:33 +0300

    hw1  added

--- seminar_proj/seminarapp/views.py
+++ seminar_proj/seminarapp/views.py
@@ -7,16 +7,18 @@ logger = logging.getLogger(__name__)
 
 def index(request):
     logger.info('Index page accessed')
-    return HttpResponse("Hello World!!!")
+    html = "<h1>Добро пожаловать!</h1>" \
+           "<p>It's django based site</p>"
+
+    return HttpResponse(html)
 
 
 def about(request):
-    try:
-        # some code that might raise an exception
-        result = 1 // 0
-    except Exception as e:
-        logger.exception(f'Error in about page: {e}')
-        return HttpResponse("Oops, something went wrong.")
-    else:
-        logger.debug('About page accessed')
-        return HttpResponse("This is the about page.")
+    logger.info('About page accessed')
+    html = "<h1>О нас:</h1>" \
+           "<p>It's django based site<br>" \
+           "This site was create for learning popular" \
+           "<br> <b>Django framework</b><br>" \
+           "because it  fun and very helpful for job search!</p>"
+
+    return HttpResponse(html)
